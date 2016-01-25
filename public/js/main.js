4b765516a8497fb218891137031a5c73751817fa
Amir Othman <th3go2@gmail.com>
2016-01-25 12:39:21 +0100

    add slideable menu

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,24 @@
-$("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-});
+$(window).load(function(){
+        
+
+
+        $("[data-toggle]").click(function() {
+
+          var toggle_el = $(this).data("toggle");
+          $(toggle_el).toggleClass("open-sidebar");
+        });
+
+         $(".swipe-area").swipe({
+              swipeStatus:function(event, phase, direction, distance, duration, fingers)
+                  {
+                      if (phase=="move" && direction =="right") {
+                           $(".container").addClass("open-sidebar");
+                           return false;
+                      }
+                      if (phase=="move" && direction =="left") {
+                           $(".container").removeClass("open-sidebar");
+                           return false;
+                      }
+                  }
+          }); 
+      });
